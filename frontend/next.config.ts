aa7fb48a4439e669c07925750528c533cbffabd0
thomas <thomas.yosh@tutanota.com>
2024-12-22 10:44:06 +0000

    shadcnui

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  env:{
+    PROJECT_NAME: process.env.PROJECT_NAME,
+  }
 };
 
 export default nextConfig;
--- frontend/src/app/demo/page.tsx
+++ frontend/src/app/demo/page.tsx
@@ -1,3 +1,38 @@
-export default function Page(){
-    return 55555;
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+  } from "@/components/ui/card"
+
+interface Recipe {
+    pk_id: number,
+    name: string
+}
+
+async function getRecipes(): Promise<Recipe[]>{
+    const result = await fetch ('http://backend:8000/')
+
+    return result.json()
+}
+
+export default async function Page(){
+    const recipes = await getRecipes()
+    return (
+        <main>
+            <div className="grid grid-cols-3 gap-8">
+                {recipes.map(recipe => (
+                    <Card key={recipe.pk_id}>
+                        <CardHeader>
+                            <div>
+                                <CardTitle>{recipe.name}</CardTitle>
+                            </div>
+                        </CardHeader>
+                    </Card>
+                ))}
+            </div>
+        </main>
+    )
 }
