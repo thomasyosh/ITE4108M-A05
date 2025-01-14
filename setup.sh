2e669eae2f6dc6e2f8f1a863f2290dac82fb70e2
thomasyosh <thomas.yosh@tutanota.com>
2025-01-14 18:43:30 +0800

    fixed shell script

--- setup.sh
+++ setup.sh
@@ -36,12 +36,6 @@ fi
 if [ "$DATABASE_TYPE" = "mysql" ] && [ "$LOCAL_DB" = "no" ]; then
     docker run -v ./backend/db/mysql_init.sh:/mysql_init.sh --env-file .env --rm mysql_init:latest ./mysql_init.sh
     docker rmi $(docker images 'mysql' -a -q)
-    echo "$DATABASE_TYPE" database init completed";
-fi
-
-if [ "$LOCAL_DB" = "yes" ]; then
-    docker run -v ./backend/db/localdb.sh:/localdb.sh --env-file .env --rm postgres:latest ./localdb.sh
-    docker rmi $(docker images 'postgres' -a -q)
     echo "$DATABASE_TYPE" database init completed;
 fi
 
